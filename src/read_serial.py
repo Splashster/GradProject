0faa68cb6d7f87609aa2f08a89a7c5469da05d46
Splashster <psotails@gmail.com>
2018-10-17 03:41:36 -0500

    Added temperature and humidity

--- src/read_serial.py
+++ src/read_serial.py
@@ -4,22 +4,28 @@ import time
 import serial
 
 ser = serial.Serial(
-	port='/dev/ttyACM0',
-        baudrate = 9600,
+	port='/dev/ttyACM3',
+        baudrate = 2000000,
         parity=serial.PARITY_NONE,
         stopbits=serial.STOPBITS_ONE,
         bytesize=serial.EIGHTBITS,
-        timeout=1
+        timeout=5
         )
-counter=0
 
 def readSerial():
-    x = ser.readline()
-    return x
+    counter=0
 
+    try:
+        x = ser.readline()
+        return x
+    except serial.serialutil.SerialException:
+        pass
+
+            
+'''
 if __name__ == "__main__":
     while 1:
         val = readSerial()
         print val
-
+'''
     
---
+++ start_outstation.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+xterm -hold -e "/home/pi/rpi-dnp3-gpio/build/rpi-dnp3-gpio /home/pi/rpi-dnp3-gpio/default.ini" 
+
