3aadfaf7c46b59238581c2d0bdb94975ebd6d080
Splash <psotails@gmail.com>
2018-10-27 05:21:49 -0500

    Documentation Time!

--- RTU/start_outstation.sh
+++ RTU/start_outstation.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/home/pi/GradProject/RTU/rpi-dnp3-gpio_clean /home/pi/GradProject/RTU/default.ini </dev/null &>/dev/null &
+/home/pi/GradProject/RTU/rpi-dnp3-gpio_clean /home/pi/GradProject/RTU/default.ini
